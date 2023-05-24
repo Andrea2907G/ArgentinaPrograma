8c0ff5981436bc28e2e6d78944a01126d4131bdf
Andrea2907g <gz5695015rx@gmail.com>
2023-05-23 22:53:46 -0300

    Programas resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,22 @@
+/*1.b Dado tres números calcule la multiplicación entre ellos y lo imprima por
+pantalla. Deberá guardar este programa en un archivo llamado:
+multiplicacion.js. Ayuda: utilizar console.log.*/
+
+
+// --> Declaración y asignación de variables
+
+let num1 = 40;
+let num2 = 35;
+let num3 = 50
+let multiplicacion = num1 * num2 * num3;
+console.log ("El resultado de multiplicar", num1,"*", num2,"*", num3, "es", multiplicacion); 
+
+/* ¿Qué valor devuelve su programa si los números son 2 *17 * 6?*/
+
+// --> Actualización de valores
+
+num1 = 2;
+num2 = 35;
+num3 = 50;
+multiplicacion = num1 * num2 * num3;
+console.log ("El resultado de multiplicar", num1,"*", num2,"*", num3, "es", multiplicacion); 
---
+++ practico0/resta.js
@@ -0,0 +1,21 @@
+/*1.a) Escribir un programa JavaScript que:
+a. Dado dos números calcule la resta entre ambos; donde el primer número
+es el minuendo y el segundo es el sustraendo. Imprima el resultado por
+pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
+Ayuda: utilizar console.log*/
+
+// --> Declaración y asignación de variables
+
+let minuendo = 400;
+let sustraendo = 350;
+let resta = minuendo - sustraendo;
+console.log ("Con minuendo", minuendo, "y", "sustraendo", sustraendo, "el resultado de la operación es", resta);
+
+//¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+
+// --> Actualización de valores
+
+minuendo = 10;
+sustraendo = 2;
+resta = minuendo - sustraendo;
+console.log("Con minuendo", minuendo, "y", "sustraendo", sustraendo, "el resultado de la operación es", resta)
