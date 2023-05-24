c5d24242d0ffcd8d5753e1a3c81e43c00310b8ef
Andrea2907g <gz5695015rx@gmail.com>
2023-05-23 22:59:18 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,28 @@
+
+/*2. Escribir un programa JavaScript que calcule el área de un rectángulo*/
+
+// -> Declaracion de variables
+
+let base;
+let altura;
+let area;
+
+
+
+//a) area del rectangulo de base 5 y altura 3.
+
+// -> Inicialización de variables
+
+base = 5;
+altura = 3;
+area = base * altura 
+console.log ("El área de un rectangulo de base", base, "y", "altura", altura, "es", area); 
+
+//b) area del rectangulo de base 12.5 y altura 5.3.
+
+// -> Actualización de valores
+
+base = 12.5;
+altura = 5.3; 
+area = base * altura
+console.log ("El área de un rectangulo de base", base, "y", "altura", altura, "es", area); 
