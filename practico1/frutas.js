89b7b44da95788548f945d82256fa6af2eebbaa2
Andrea2907g <gz5695015rx@gmail.com>
2023-06-07 01:32:10 -0300

    5. Programa frutas.js

---
+++ practico1/frutas.js
@@ -0,0 +1,29 @@
+//PARTE 2: COMANDOS BÁSICOS
+
+//Punto 5 Manejo de arreglos:
+
+//a.
+
+let frutas = [];
+
+//b.
+
+let fruta1 = "Granada";
+let fruta2 = "Pera";
+let fruta3 = "Durazno";
+let fruta4 = "Tomate";
+let fruta5 = "Manzana";
+
+frutas.push(fruta1, fruta2, fruta3, fruta4, fruta5);
+
+//c. 
+
+let ultima = frutas.pop();
+console.log("La ultima fruta agregada fue: %s", ultima);
+
+//d.
+
+let primera = frutas.shift();
+console.log("La primera fruta agregada fue: %s", primera);
+
+
