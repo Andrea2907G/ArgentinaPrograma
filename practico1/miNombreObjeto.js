0c4aba2fe0224822974218c07530890a27adff2c
Andrea2907g <gz5695015rx@gmail.com>
2023-06-07 01:27:47 -0300

    2. Programa miNombreObjeto

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,9 @@
+//PARTE 1: VARIABLES
+
+//Punto 2:
+
+//a. 
+let persona = {nombre: "Andrea", apellido: "Gonzalez", edad: 35};
+
+//b.
+console.log("Mi objeto persona es el siguiente:" + JSON.stringify(persona));
