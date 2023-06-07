6035398639db1256665f6bc564a3d33eff9e71f5
Andrea2907g <gz5695015rx@gmail.com>
2023-06-07 01:29:56 -0300

    3. Programa operacionesAritmeticas

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,33 @@
+//PARTE 2: COMANDOS BÁSICOS
+
+//Punto 3  Operadores aritméticos:
+
+//a.
+
+let num1 = 6;
+let num2 = -2;
+let num3 = 33;
+let num4 = 22;
+
+//b.
+
+sumaNum1Num2 = num1 + num2;
+
+//c.
+
+restaNum3Num4 = num3 - num4;
+
+//d.
+
+resultadoFinal = sumaNum1Num2 * restaNum3Num4;
+
+//e. 
+
+let esPar = resultadoFinal % 2 === 0;
+
+//f. 
+
+console.log("Mis variables iniciales fueron: %i, %i, %i y %i. La respuesta a verificar si el resultado final es par es: %s", num1, num2, num3, num4, esPar);
+
+
+
