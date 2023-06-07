f0f8df33d65e84e825609c487a390aceabd94c66
Andrea2907g <gz5695015rx@gmail.com>
2023-06-07 01:22:47 -0300

    programa miNombreCompleto

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,16 @@
+//PARTE 1: VARIABLES
+
+//Punto 1:
+
+//a.
+let nombre;
+//b.
+nombre = "Andrea";
+//c.
+let apellido = "Gonzalez";
+//d.
+let edad = 35;
+//e.
+console.log("Hola, %s %s. Tienes %i años.", nombre, apellido, edad);
+
+
