4ccfc5ca4b57ea819f3fd13ebace56c782e2e1a0
Andrea2907g <gz5695015rx@gmail.com>
2023-06-22 21:44:55 -0300

    Programa operadoresLogicos

--- practico2/operadoresLogicos.js
+++ practico2/operadoresLogicos.js
@@ -1,9 +1,8 @@
 //Ejercicio 3
 
 const readlineSync = require('readline-sync');
-let valorEntero = readlineSync.question('Ingrese un numero:');
-valorEntero = Number(valorEntero);
-valorEntero = parseInt(valorEntero); 
+let valorEntero = readlineSync.question('Ingrese un numero entero: ');
+valorEntero = parseInt(valorEntero); // Extrae la parte entera de un flotante y convierte un string a tipo numerico, util para comparación estricta.
 if ((valorEntero > 0) && (valorEntero % 2 === 0)) {
     console.log("El numero es positivo y par");
 } else if ((valorEntero > 0) && (valorEntero % 2 !== 0)){
